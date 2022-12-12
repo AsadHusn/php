f161b48b92fd363981390a18123f143fbc5b1769
Axad <asadhusn007@gmail.com>
2022-12-12 23:48:24 +0500

    changes

---
+++ Feedback/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <h2>Traversy Media</h2>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Feedback</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <img src="logo.png" width="100px">
+        <h1>Feedback</h1>
+        <h4>Leave feedback for Traversy Media</h4>
+
+        <form>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Enter your name">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Enter your email">
+            <label>Feedback</label>
+            <textarea name="feedback" cols="30" rows="5" placeholder="Enter your feedback"></textarea>
+            <input type="submit" value="Send">
+        </form>
+    </main>
+
+    <footer>
+        <small>Copyright &copy; 2022</small>
+    </footer>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
 
+class User{
+    function __construct($name, $email){
+        $this->name = $name;
+        $this->email = $email;
+    }
+}
 
+class Employee extends User{
+    private $pass;
+    function __construct($name, $email, $title, $pass){
+        parent::__construct($name, $email);
+        $this->title = $title;
+        $this->pass = $pass;
+    }
+}
 
+$emp = new Employee("Asad", "asad@gmail.com", "Manager", '123');
 
-
-
-
-
+var_export( $emp);
 
 
 ?>
